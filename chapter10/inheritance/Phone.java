49216707776ea3cd5fd8f5532ab98f7dbbdc60fa
Hyun <ghkdgus29@naver.com>
2023-03-23 21:45:01 +0900

    오브젝트 ch10 코드

---
+++ chapter10/inheritance/Phone.java
@@ -0,0 +1,29 @@
+package chapter10.inheritance;
+
+import chapter2.good.Money;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Phone {
+
+    private double taxRate;
+
+    private List<Call> calls = new ArrayList<>();
+
+    public Phone(double taxRate) {
+        this.taxRate = taxRate;
+    }
+
+    public Money calculateFee() {
+        Money result = Money.ZERO;
+
+        for (Call call : calls) {
+            result = result.plus(calculateCallFee(call));
+        }
+
+        return result;
+    }
+
+    abstract protected Money calculateCallFee(Call call);
+}
---
+++ chapter10/inheritance/RegularPhone.java
@@ -0,0 +1,22 @@
+package chapter10.inheritance;
+
+import chapter2.good.Money;
+
+import java.time.Duration;
+
+public class RegularPhone extends Phone{
+
+    private Money amount;
+    private Duration seconds;
+
+    public RegularPhone(Money amount, Duration seconds, double taxRate) {
+        super(taxRate);
+        this.amount = amount;
+        this.seconds = seconds;
+    }
+
+    @Override
+    protected Money calculateCallFee(Call call) {
+        return amount.times(call.getDuration().getSeconds() / seconds.getSeconds());
+    }
+}
